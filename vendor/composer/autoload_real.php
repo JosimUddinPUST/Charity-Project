f72fc94db1741b051f9720b4850dfe9037a62b94
Md Josim Uddin <95582659+JosimUddinPUST@users.noreply.github.com>
2026-04-12 08:57:16 +0600

    Add some feature files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInite56e4243ecb436c99f783e3df434a882
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInite56e4243ecb436c99f783e3df434a882', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInite56e4243ecb436c99f783e3df434a882', 'loadClassLoader'));
